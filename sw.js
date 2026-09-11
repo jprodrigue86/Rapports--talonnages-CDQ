1c1c8c6c27b4ae84a98ec383847c32ceac21a2c7
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-10 22:52:15 -0400

    Update service worker cache and add message event

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'cdq-installable-heavy-v6-ui';
-const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './sw.js',
+const CACHE = 'cdq-installable-heavy-v8-202609102017';
+const APP_SHELL = ['./', './index.html', './manifest.webmanifest', './sw.js', './version.json',
   './icons/icon-heavy-v3-192.png', './icons/icon-heavy-v3-512.png'];
 
 self.addEventListener('install', event => {
@@ -12,6 +12,10 @@ self.addEventListener('activate', event => {
     caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
   self.clients.claim();
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
