44284fda023cf98cb67e25be1d3fae1a143e0ea2
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-15 14:35:25 -0400

    V21.37: rafraîchir le cache PWA pour Balance à camion

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // GitHub/PWA uniquement. Ne jamais coller ce fichier dans Code.gs.
-const CACHE = 'cdq-installable-v21-37';
+const CACHE = 'cdq-installable-v21-37-camion-1';
 const FORCE_BUILD = '2026.09.15.1530-v21.37';
 const SCOPE = new URL(self.registration.scope);
 const APP_SHELL = [
