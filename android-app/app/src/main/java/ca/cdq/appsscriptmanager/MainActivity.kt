410c8bd0947e07b545975a4ae2fc058b5d039276
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-17 11:03:22 -0400

    Add Android launcher activity using Chrome Custom Tabs

---
+++ android-app/app/src/main/java/ca/cdq/appsscriptmanager/MainActivity.kt
@@ -0,0 +1,40 @@
+package ca.cdq.appsscriptmanager
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import androidx.browser.customtabs.CustomTabsIntent
+
+class MainActivity : Activity() {
+    private val appUrl = Uri.parse("https://jprodrigue86.github.io/Rapports--talonnages-CDQ/apps-script-manager/?from=android-apk")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        openManager()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        openManager()
+    }
+
+    private fun openManager() {
+        val customTabs = CustomTabsIntent.Builder()
+            .setShowTitle(false)
+            .setUrlBarHidingEnabled(true)
+            .setToolbarColor(Color.rgb(7, 17, 31))
+            .setNavigationBarColor(Color.rgb(5, 10, 18))
+            .build()
+
+        customTabs.intent.setPackage("com.android.chrome")
+
+        try {
+            customTabs.launchUrl(this, appUrl)
+        } catch (_: ActivityNotFoundException) {
+            startActivity(Intent(Intent.ACTION_VIEW, appUrl))
+        }
+    }
+}
