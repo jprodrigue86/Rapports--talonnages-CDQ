3b3c77845e42b9cba3bc127187f748a7a4bc3070
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-18 09:23:08 -0400

    Update Script Manager cache to V18

--- apps-script-manager/sw.js
+++ apps-script-manager/sw.js
@@ -1,5 +1,5 @@
-const CACHE='cdq-apps-script-manager-v17';
-const ASSETS=['./','index.html','style.css?v=17','api.js?v=17','jszip.min.js?v=17','app.js?v=17','manifest.webmanifest','icon-192.png','icon-512.png'];
+const CACHE='cdq-apps-script-manager-v18';
+const ASSETS=['./','index.html','style.css?v=18','api.js?v=18','jszip.min.js?v=18','app.js?v=18','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cdq-apps-script-manager-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
