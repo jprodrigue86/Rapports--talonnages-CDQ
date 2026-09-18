ce4e89803ab2fe3e7f2503d1b71e3988d8d6507d
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-18 16:31:30 -0400

    CDQ Script Manager V25: cache bundles patch

--- apps-script-manager/sw.js
+++ apps-script-manager/sw.js
@@ -1,4 +1,4 @@
-const CACHE='cdq-apps-script-manager-v24-direct-bundle';
+const CACHE='cdq-apps-script-manager-v25-direct-patches';
 const ASSETS=['./','index.html','style.css?v=23','api.js?v=23','jszip.min.js?v=23','app.js?v=23','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cdq-apps-script-manager-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
