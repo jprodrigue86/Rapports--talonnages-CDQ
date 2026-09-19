d900dd2cc5452818ab4791eaffeeefd081b285bd
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-19 16:52:22 -0400

    Add balance-cdq-android/app/build.gradle.kts for Balance CDQ Android native PDF flow

---
+++ balance-cdq-android/app/build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "ca.balancecdq.android"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "ca.balancecdq.android"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 2273
+        versionName = "22.73"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.browser:browser:1.8.0")
+    implementation("com.google.android.gms:play-services-auth:21.3.0")
+}
