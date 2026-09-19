fdf17cf3bc70ae2e52b3d5dc85d946ef5d57ac94
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-18 21:05:20 -0400

    Script Manager V29: refresh diagnostic cache

--- apps-script-manager/sw.js
+++ apps-script-manager/sw.js
@@ -1,5 +1,5 @@
-const CACHE='cdq-apps-script-manager-v28-resilient-bundles';
-const ASSETS=['./','index.html','style.css?v=23','api.js?v=23','jszip.min.js?v=23','app.js?v=28','manifest.webmanifest','icon-192.png','icon-512.png'];
+const CACHE='cdq-apps-script-manager-v29-deployment-diagnostic';
+const ASSETS=['./','index.html','style.css?v=23','api.js?v=23','jszip.min.js?v=23','app.js?v=29','manifest.webmanifest','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cdq-apps-script-manager-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
