d1c1c6c070143f586bf0cab5fb997aa8a7015f39
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-21 14:23:54 -0400

    Balance CDQ Android 25.02 share installed APK directly

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2501
-        versionName = "25.01"
+        versionCode = 2502
+        versionName = "25.02"
     }
 
     buildTypes {
