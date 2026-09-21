8b1a1ffb0530ce3971a88deb1178a6874aa29610
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-21 10:17:38 -0400

    Bump Script Manager service worker to V39

--- apps-script-manager/sw.js
+++ apps-script-manager/sw.js
@@ -1,5 +1,5 @@
-const CACHE='cdq-apps-script-manager-v38-no-left-rail';
-const ASSETS=['./','index.html','style.css?v=38','api.js?v=23','jszip.min.js?v=23','app.js?v=38','manifest.webmanifest','icon-industrial.svg','icon-192.png','icon-512.png'];
+const CACHE='cdq-apps-script-manager-v39-resilient-google-api';
+const ASSETS=['./','index.html','style.css?v=39','api.js?v=39','jszip.min.js?v=23','app.js?v=39','manifest.webmanifest','icon-industrial.svg','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('cdq-apps-script-manager-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
