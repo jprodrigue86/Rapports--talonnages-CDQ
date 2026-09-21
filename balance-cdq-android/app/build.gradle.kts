da9bf32c34fb1f2a06619e2f4af281503d1b0741
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-21 13:52:47 -0400

    Balance CDQ Android 25.00 complete native PDF workflow

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2405
-        versionName = "24.05"
+        versionCode = 2500
+        versionName = "25.00"
     }
 
     buildTypes {
