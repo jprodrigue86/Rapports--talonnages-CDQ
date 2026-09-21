99e35e5fe9b09a6b7c124a3eebf1cecc7fb9d633
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-21 14:37:56 -0400

    Bump Balance CDQ Android to 25.03 native app shell

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2502
-        versionName = "25.02"
+        versionCode = 2503
+        versionName = "25.03"
     }
 
     buildTypes {
