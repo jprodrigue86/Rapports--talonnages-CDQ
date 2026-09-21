dba8d62d4b907ac1d075a06156dae03a530a1e7f
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-21 15:06:05 -0400

    Bump Balance CDQ Android to 25.05

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2504
-        versionName = "25.04"
+        versionCode = 2505
+        versionName = "25.05"
     }
 
     buildTypes {
