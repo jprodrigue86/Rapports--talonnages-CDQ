e79845591ba7ea36756b540885b2555b3cf3a624
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-25 16:06:44 -0400

    Restore pre-fast-start V25.36 application baseline

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2538
-        versionName = "25.38"
+        versionCode = 2536
+        versionName = "25.36"
     }
 
     buildTypes {
