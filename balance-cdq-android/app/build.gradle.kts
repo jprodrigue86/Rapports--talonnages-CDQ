1e0f996a18e0ce477d3e85202144dbd2d3c4cd4d
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-25 16:11:08 -0400

    Bump clean fast-start retry to V25.39

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2536
-        versionName = "25.36"
+        versionCode = 2539
+        versionName = "25.39"
     }
 
     buildTypes {
