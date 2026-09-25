3f240b0a62f193eca78083661000e73fe9f888de
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-25 14:28:50 -0400

    Bump Balance CDQ to V25.38 persistent fast start

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2537
-        versionName = "25.37"
+        versionCode = 2538
+        versionName = "25.38"
     }
 
     buildTypes {
