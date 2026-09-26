7d5184b16d3f22f579a2a281bdf47ac356a12350
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-25 21:57:15 -0400

    V25.43: bump Android version

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2542
-        versionName = "25.42"
+        versionCode = 2543
+        versionName = "25.43"
     }
 
     buildTypes {
