c0f466085125108b73377277c0f6e6b1a5a7b376
jprodrigue86 <jp.rodrigue86@gmail.com>
2026-09-26 14:10:45 -0400

    V25.51: bump Android release identity

--- balance-cdq-android/app/build.gradle.kts
+++ balance-cdq-android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "ca.balancecdq.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2550
-        versionName = "25.50"
+        versionCode = 2551
+        versionName = "25.51"
     }
 
     buildTypes {
